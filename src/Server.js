87db82dd0eb2b340a121d6b616d923c85865dad3
daniellourenco-Samba <daniel.lourenco@sambatech.com>
2024-08-13 15:55:22 -0300

    feat: deploy to heroku

--- src/Server.js
+++ src/Server.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
-});
+const io = new Server(server);
 
 let users = {};
 
@@ -52,6 +48,13 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(4000, () => {
-  console.log('listening on *:4000');
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- src/client/index.js
+++ src/client/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { VoteProvider } from './store'; // Importando o VoteProvider
+import { VoteProvider } from '../store'; // Importando o VoteProvider
 
 ReactDOM.render(
   <React.StrictMode>
